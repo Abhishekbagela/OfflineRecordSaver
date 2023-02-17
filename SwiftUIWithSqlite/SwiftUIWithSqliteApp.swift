b14452e2b92cd8a1decbe2354f261cb1fae12c67
Abhishek-ab@ios-dev <abhibagela2015@gmail.com>
2023-02-17 23:49:38 +0530

    Initial Commit

---
+++ SwiftUIWithSqlite/SwiftUIWithSqliteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithSqliteApp.swift
+//  SwiftUIWithSqlite
+//
+//  Created by Abhishek Bagela on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithSqliteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
